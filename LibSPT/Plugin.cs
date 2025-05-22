b2671fa3bca864b0922a619f9dc9258c2fde4ca1
SleepingPills <SleepingPills@users.noreply.github.com>
2025-05-22 12:52:18 +0100

    Project

---
+++ LibSPT/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace HollywoodCam;
+
+[BepInPlugin("com.janky.hollywoodcam", "Janky's HollywoodCam", HollywoodCamVersion)]
+[SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Evident")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+public class Plugin : BaseUnityPlugin
+{
+    public const string HollywoodCamVersion = "1.0.0";
+
+    public static ManualLogSource Log;
+
+    private static ConfigEntry<bool> _loggingEnabled;
+
+    private void Awake()
+    {
+        Log = Logger;
+        
+        SetupConfig();
+
+        if (_loggingEnabled.Value)
+        {
+            Log.LogInfo("Logging enabled");
+        }
+        else
+        {
+            Log.LogInfo("Logging disabled");
+            BepInEx.Logging.Logger.Sources.Remove(Log);
+        }
+    }
+
+    private void SetupConfig()
+    {
+        const string debug = "Debug";
+
+        _loggingEnabled = Config.Bind(debug, "Enable Debug Logging", true, new ConfigDescription(
+            "Duh. Requires restarting the game to take effect.",
+            null,
+            new ConfigurationManagerAttributes { Order = 1 }
+        ));
+    }
+}
