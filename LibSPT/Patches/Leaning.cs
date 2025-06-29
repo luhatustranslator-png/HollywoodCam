f520ef214e1f248442fbc6feeddf451a950f6881
SleepingPills <SleepingPills@users.noreply.github.com>
2025-06-29 14:16:13 +0100

    Reorg for better naming

---
+++ LibSPT/Patches/Leaning.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using EFT;
+using SPT.Reflection.Patching;
+
+namespace HollywoodCam.Patches;
+
+public class PlayerOnLeanPostfixPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(Player).GetMethod(nameof(Player.method_3));
+    }
+
+    [PatchPostfix]
+    private static void Postfix(Player __instance, float dir)
+    {
+        if (Plugin.GunStanceSync.Value != GunStanceSyncEnum.Lean || !__instance.IsYourPlayer || __instance.MovementContext == null)
+            return;
+
+        var firearmController = __instance.HandsController as Player.FirearmController;
+
+        // Don't shoulder swap in first person ADS mode
+        if (firearmController == null || (firearmController.IsAiming && __instance.PointOfView == EPointOfView.FirstPerson))
+            return;
+
+        if ((__instance.MovementContext.LeftStanceEnabled && dir > 0f) || (!__instance.MovementContext.LeftStanceEnabled && dir < 0f))
+            firearmController.ChangeLeftStance();
+    }
+}
---
+++ LibSPT/Patches/Lifecycle.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using EFT;
+using SPT.Reflection.Patching;
+using UnityEngine;
+
+namespace HollywoodCam.Patches;
+
+public class GameWorldStartedPostfixPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+    }
+
+    [PatchPostfix]
+    // ReSharper disable once InconsistentNaming
+    public static void Postfix(GameWorld __instance)
+    {
+        var tpView = __instance.MainPlayer.gameObject.AddComponent<ThirdPersonView>();
+        tpView.localPlayer = __instance.MainPlayer;
+
+        // Disables the jitter when rotating on the trunk
+        if (!Plugin.ShimmyEnabled.Value)
+            __instance.MainPlayer.TrunkRotationLimit = 0f;
+        
+        Plugin.Log.LogInfo("Third Person Camera & Player Initialized");
+    }
+}
+
+public class PlayerDisposePrefixPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(Player).GetMethod(nameof(Player.Dispose));
+    }
+
+    [PatchPrefix]
+    // ReSharper disable once InconsistentNaming
+    public static void Postfix(Player __instance)
+    {
+        if (!__instance.IsYourPlayer)
+            return;
+
+        var tpView = __instance.gameObject.GetComponent<ThirdPersonView>();
+
+        if (tpView == null)
+        {
+            Plugin.Log.LogInfo("Player.Dispose: ThirdPersonView has already been destroyed");
+            return;
+        }
+
+        Object.DestroyImmediate(tpView);
+        Plugin.Log.LogInfo("Player.Dispose: ThirdPersonView destroyed successfully");
+    }
+}
+
+public class PlayerOnDeadPrefixPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(Player).GetMethod(nameof(Player.OnDead));
+    }
+
+    [PatchPrefix]
+    // ReSharper disable once InconsistentNaming
+    public static void Postfix(Player __instance)
+    {
+        if (!__instance.IsYourPlayer)
+            return;
+
+        var tpView = __instance.gameObject.GetComponent<ThirdPersonView>();
+
+        if (tpView == null)
+        {
+            Plugin.Log.LogInfo("Player.OnDead: ThirdPersonView has already been destroyed");
+            return;
+        }
+
+        Object.DestroyImmediate(tpView);
+        Plugin.Log.LogInfo("Player.OnDead: ThirdPersonView destroyed");
+    }
+}
