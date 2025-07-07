7e593a9120706b4c8c32a7fb544d19c321abf276
SleepingPills <SleepingPills@users.noreply.github.com>
2025-07-07 20:17:17 +0100

    Stripped out the grenade assist

--- LibSPT/Plugin.cs
+++ LibSPT/Plugin.cs
@@ -2,7 +2,6 @@
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
-using HollywoodCam.GrenadeAssist;
 using HollywoodCam.Patches;
 using UnityEngine;
 
@@ -45,7 +44,7 @@ public enum AdsModeEnum
 [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 public class Plugin : BaseUnityPlugin
 {
-    public const string HollywoodCamVersion = "1.1.0";
+    public const string HollywoodCamVersion = "1.1.1";
 
     public static ManualLogSource Log;
 
@@ -86,14 +85,6 @@ public class Plugin : BaseUnityPlugin
     public static ConfigEntry<bool> ShimmyEnabled;
     public static ConfigEntry<float> InteractionRange;
     
-    public static ConfigEntry<bool> GrenadeArcEnabled;
-    public static ConfigEntry<float> GrenadeArcDistance;
-    public static ConfigEntry<Color> GrenadeArcStartColor;
-    public static ConfigEntry<Color> GrenadeArcEndColor;
-    public static ConfigEntry<Color> GrenadeArcKnobColor;
-    public static ConfigEntry<float> GrenadeArcKnobSize;
-    public static ConfigEntry<float> GrenadeArcResolution;
-
     public static ConfigEntry<bool> DebugUIEnabled;
     private static ConfigEntry<bool> _loggingEnabled;
 
@@ -134,12 +125,6 @@ public class Plugin : BaseUnityPlugin
         // Interaction
         new GameWorldFindInteractablePrefixPatch().Enable();
         new PlayerInteractionRayPrefixPatch().Enable();
-
-        // Grenade
-        new GrenadeSetThrowForcePrefixPatch().Enable();
-        new GrenadeAssistGameWorldStartedPostfixPatch().Enable();
-        new GrenadeAssistPlayerDisposePrefixPatch().Enable();
-        new GrenadeAssistPlayerOnDeadPrefixPatch().Enable();
 
         if (_loggingEnabled.Value)
         {
@@ -320,38 +305,6 @@ public class Plugin : BaseUnityPlugin
 
         // Note: this is the overlap threshold where ADSing is prevented. Default is 0.
         EFTHardSettings.Instance.STOP_AIMING_AT = 100f;
-
-        GrenadeArcEnabled = Config.Bind(headerAssist, "Enable Grenade Assist", true, new ConfigDescription(
-            "Toggles a visual grenade assist - mostly for helping with 3rd person aiming.",
-            tags: new ConfigurationManagerAttributes { Order = 7 }
-        ));
-        GrenadeArcDistance = Config.Bind(headerAssist, "Max Grenade Assist Distance", 25f, new ConfigDescription(
-            "How far to draw the grenade assist arc.",
-            new AcceptableValueRange<float>(1f, 50f),
-            tags: new ConfigurationManagerAttributes { Order = 6 }
-        ));
-        GrenadeArcStartColor = Config.Bind(headerAssist, "Grenade Arc Start Color", new Color(0, 1, 0, 0f), new ConfigDescription(
-            "Color of the start of the grenade arc. Make sure to configure the alpha value correctly so that you get a nice fade.",
-            tags: new ConfigurationManagerAttributes { Order = 5 }
-        ));
-        GrenadeArcEndColor = Config.Bind(headerAssist, "Grenade Arc End Color", new Color(1, 0, 0, 0.75f), new ConfigDescription(
-            "Color of the end of the grenade arc. Make sure to configure the alpha value correctly so that you get a nice fade.",
-            tags: new ConfigurationManagerAttributes { Order = 4 }
-        ));
-        GrenadeArcKnobColor = Config.Bind(headerAssist, "Grenade Arc Knob Color", new Color(1, 0, 0, 0.75f), new ConfigDescription(
-            "The uh, color of the knob. At the end of the big curved thing. Knob.",
-            tags: new ConfigurationManagerAttributes { Order = 3 }
-        ));
-        GrenadeArcKnobSize = Config.Bind(headerAssist, "Grenade Arc Knob Size", 0.2f, new ConfigDescription(
-            "Giggity.",
-            new AcceptableValueRange<float>(0.01f, 1f),
-            tags: new ConfigurationManagerAttributes { Order = 2 }
-        ));
-        GrenadeArcResolution = Config.Bind(headerAssist, "Max Grenade Assist Resolution", 0.5f, new ConfigDescription(
-            "Step size when calculating the grenade arc. Too small values take exponentially more time to compute and too large values become inaccurate.",
-            new AcceptableValueRange<float>(0.1f, 2f),
-            tags: new ConfigurationManagerAttributes { Order = 1, IsAdvanced = true}
-        ));
 
         DebugUIEnabled = Config.Bind(headerDebug, "Enable Debug UI", false, new ConfigDescription(
             "Enables the debug UI for diagnosing common issues.",
