48d3c7d1b0c20fcf7c14b41c417eb72215c70c61
SleepingPills <SleepingPills@users.noreply.github.com>
2025-07-27 12:46:59 +0100

    up v

--- LibSPT/Plugin.cs
+++ LibSPT/Plugin.cs
@@ -44,7 +44,7 @@ public enum AdsModeEnum
 [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 public class Plugin : BaseUnityPlugin
 {
-    public const string HollywoodCamVersion = "1.1.3";
+    public const string HollywoodCamVersion = "1.1.4";
 
     public static ManualLogSource Log;
 
